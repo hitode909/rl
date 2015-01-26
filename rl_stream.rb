f9177dfd67401aefff4d931b2942316a0682c503
hitode909 <hitode909@gmail.com>
2015-01-26 10:07:05 +0900

    stream

---
+++ rl_stream.rb
@@ -0,0 +1,18 @@
+require './lib/rl'
+
+rl = RL.new
+
+all_lefts = []
+all_rights = []
+ARGF.each_line{|line|
+  term = line.chomp
+  l, r = *(rl.detect term)
+  all_rights << term if r == 0
+  all_lefts << term if l == 0
+}
+
+puts '## All rights'
+puts '> ' + all_rights.sort_by{|a| -a.length}[0..100].join(' ')
+puts
+puts '## All lefts'
+puts '> ' + all_lefts.sort_by{|a| -a.length}[0..100].join(' ')
